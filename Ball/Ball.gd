2c4407a8c1a87c8ed78e6d7fd5f0bcd5da4b8cf3
Tony Marklove <tony@tonymarklove.net>
2018-08-02 17:45:16 +0100

    Basic ball control

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,9 +1,17 @@
 extends KinematicBody2D
 
+const FRICTION = 0.8
+const MIN_SPEED = 0.01
+
 var velocity = Vector2(0,0)
 
 func _physics_process(delta):
-	var collision = move_and_collide(velocity)
+	move_and_collide(velocity)
 
-	if collision:
-		print("ball collided")
+	velocity = velocity * (1 - (FRICTION * delta))
+
+	if velocity.length() < MIN_SPEED:
+		velocity = Vector2()
+
+func kick(velocity):
+	self.velocity = velocity
--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ var velocity = Vector2(0,0)
 const STARTING_VECTOR = Vector2(0,-1)
 const JOYSTICK_DEADZONE = 0.2
 const PLAYER_SPEED = 5
+const KICK_POWER = 2
 
 onready var ball = get_node("/root/World/Ball")
 onready var ball_trajectory = get_node("BallTrajectory")
@@ -41,16 +42,17 @@ func _physics_process(delta):
 	else:
 		velocity = Vector2()
 
+	if !look_vector:
+		look_vector = move_velocity
+
 	if look_vector:
 		self.rotation = -look_vector.angle_to(STARTING_VECTOR)
-	elif move_velocity:
-		self.rotation = -move_velocity.angle_to(STARTING_VECTOR)
 
 	# Set position of ball trajectory indicator
 	ball_trajectory.rotation = self.rotation
 	ball_trajectory.position = ball.position
 
-	var collision = move_and_collide(velocity)
+	move_and_collide(velocity)
 
-	if collision:
-		print("collided")
+	if look_vector && (ball.position - position).length() <= 32:
+		ball.kick(velocity + look_vector * KICK_POWER)
